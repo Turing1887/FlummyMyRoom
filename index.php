858f06aaa54618eaa512886a25f661e7419dfb51
Turing1887 <tim.zschage@t-online.de>
2016-10-09 09:03:25 +0200

    add all da tings

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>FlummyMyRoom</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!--<link rel="stylesheet" href="css/normalize.css">-->
+        <link rel="stylesheet" href="css/style.css">
+      <link rel="shortcut icon" href="img/faviconFl.ico" type="image/x-icon">
+<link rel="icon" href="img/faviconFl.ico" type="image/x-icon">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <section class="main">
+          <div class="content">
+            <h2>Sind sie: </h2>
+            <button class="mieterBtn">Mieter</button>
+            <button id="vermieter-btn" class="vermieterBtn">Vermieter</button>
+          </div>
+        </section>
+
+
+        <!--<script src="js/plugins.js"></script>-->
+        <script src="js/main.js"></script>
+    </body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var result;
 $(document).ready(function(){
     $("#vermieter-btn").click(function(){
-        $(".main").css("display","none");
-        $(".menuVermieter").css("display","block");
+        // $(".main").css("display","none");
+        // $(".menuVermieter").css("display","block");
+        window.location.href = "vermieter.php";
+
     });
     $("#qm-btn").click(function(){
-        $(".calculator").css("display", "block");
-        $(".menuVermieter").css("display","none");
+        // $(".calculator").css("display", "block");
+        // $(".menuVermieter").css("display","none");
+        window.location.href = "calculator.php";
     });
     $("#calc-btn").click(function(){
         qmCalc();
@@ -20,7 +23,7 @@ function qmCalc(){
     var value = $("#qm-val").val();
     var preis = 12;
 
-    result = value * preis; 
+    result = value * preis;
 
 
 }
